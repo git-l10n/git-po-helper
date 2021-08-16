71e2e6fcd01d7b91f79cf10fee56826ef0f7c4e9
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2021-08-16 15:18:18 +0800

    t0041: check typos in po file when check-commits
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

---
+++ test/t0041-check-commits-with-typos.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+
+test_description="test git-po-helper check-commits with typos"
+
+. ./lib/sharness.sh
+
+HELPER="git-po-helper --no-gettext-back-compatible"
+
+test_expect_success "setup" '
+	git clone "$PO_HELPER_TEST_REPOSITORY" workdir &&
+	test -f workdir/po/git.pot &&
+	git -C workdir tag -m v1 v1
+'
+
+test_expect_success "create po/zh_CN with typos" '
+	(
+		cd workdir &&
+		cat >po/zh_CN.po <<-\EOF &&
+		msgid ""
+		msgstr ""
+		"Project-Id-Version: Git\n"
+		"Report-Msgid-Bugs-To: Git Mailing List <git@vger.kernel.org>\n"
+		"POT-Creation-Date: 2021-03-04 22:41+0800\n"
+		"PO-Revision-Date: 2021-03-04 22:41+0800\n"
+		"Last-Translator: Automatically generated\n"
+		"Language-Team: none\n"
+		"Language: zh_CN\n"
+		"MIME-Version: 1.0\n"
+		"Content-Type: text/plain; charset=UTF-8\n"
+		"Content-Transfer-Encoding: 8bit\n"
+		"Plural-Forms: nplurals=2; plural=(n != 1);\n"
+
+		msgid "exit code $res from $command is < 0 or >= 128"
+		msgstr "命令的退出码res 应该 < 0 或 >= 128"
+
+		msgid ""
+		"Unable to find current ${remote_name}/${branch} revision in submodule path "
+		"${sm_path}"
+		msgstr ""
+		"无法在子模块路径 sm_path 中找到当前的 远程/分支 版本"
+		EOF
+
+		git add "po/zh_CN.po" &&
+		test_tick &&
+		git commit -s -m "l10n: add po/zh_CN" &&
+		git tag -m v2 v2
+	)
+'
+
+cat >workdir/expect <<-\EOF
+level=info msg="[po/zh_CN.po@8907987]    2 translated messages."
+level=warning msg="[po/zh_CN.po@8907987]    mismatch variable names: $branch, $remote_name, $sm_path, sm_path"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgid: Unable to find current ${remote_name}/${branch} revision in submodule path ${sm_path}"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgstr: 无法在子模块路径 sm_path 中找到当前的 远程/分支 版本"
+level=warning
+level=warning msg="[po/zh_CN.po@8907987]    mismatch variable names: $command, $res"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgid: exit code $res from $command is < 0 or >= 128"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgstr: 命令的退出码res 应该 < 0 或 >= 128"
+level=warning
+level=warning msg="commit <OID>: author (A U Thor <author@example.com>) and committer (C O Mitter <committer@example.com>) are different"
+level=info msg="checking commits: 1 passed."
+EOF
+
+test_expect_success "check-commits show typos" '
+	(
+		cd workdir &&
+		$HELPER check-commits v1.. >out 2>&1 &&
+		make_user_friendly_and_stable_output <out >actual &&
+		test_cmp expect actual
+	)
+'
+
+cat >workdir/expect <<-\EOF
+level=info msg="[po/zh_CN.po@8907987]    2 translated messages."
+level=error msg="[po/zh_CN.po@8907987]    mismatch variable names: $branch, $remote_name, $sm_path, sm_path"
+level=error msg="[po/zh_CN.po@8907987]    >> msgid: Unable to find current ${remote_name}/${branch} revision in submodule path ${sm_path}"
+level=error msg="[po/zh_CN.po@8907987]    >> msgstr: 无法在子模块路径 sm_path 中找到当前的 远程/分支 版本"
+level=error
+level=error msg="[po/zh_CN.po@8907987]    mismatch variable names: $command, $res"
+level=error msg="[po/zh_CN.po@8907987]    >> msgid: exit code $res from $command is < 0 or >= 128"
+level=error msg="[po/zh_CN.po@8907987]    >> msgstr: 命令的退出码res 应该 < 0 或 >= 128"
+level=error
+level=warning msg="commit <OID>: author (A U Thor <author@example.com>) and committer (C O Mitter <committer@example.com>) are different"
+level=error msg="checking commits: 0 passed, 1 failed."
+
+ERROR: fail to execute "git-po-helper check-commits"
+EOF
+
+test_expect_success "check-commits show typos (--report-typos-as-errors)" '
+	(
+		cd workdir &&
+		test_must_fail $HELPER check-commits --report-typos-as-errors v1.. >out 2>&1 &&
+		make_user_friendly_and_stable_output <out >actual &&
+		test_cmp expect actual
+	)
+'
+
+test_expect_success "update po/TEAMS" '
+	(
+		cd workdir &&
+		echo >>po/TEAMS &&
+		git add -u &&
+		test_tick &&
+		git commit -s -m "l10n: TEAMS: update for test"
+	)
+'
+
+cat >workdir/expect <<-\EOF
+level=error msg="commit <OID>: bad syntax at line 79 (unknown key \"Respository\"): Respository:    https://github.com/l10n-tw/git-po"
+level=error msg="commit <OID>: bad syntax at line 80 (need two tabs between k/v): Leader:     Yi-Jyun Pan <pan93412 AT gmail.com>"
+level=warning msg="commit <OID>: author (A U Thor <author@example.com>) and committer (C O Mitter <committer@example.com>) are different"
+level=info msg="[po/zh_CN.po@8907987]    2 translated messages."
+level=warning msg="[po/zh_CN.po@8907987]    mismatch variable names: $branch, $remote_name, $sm_path, sm_path"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgid: Unable to find current ${remote_name}/${branch} revision in submodule path ${sm_path}"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgstr: 无法在子模块路径 sm_path 中找到当前的 远程/分支 版本"
+level=warning
+level=warning msg="[po/zh_CN.po@8907987]    mismatch variable names: $command, $res"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgid: exit code $res from $command is < 0 or >= 128"
+level=warning msg="[po/zh_CN.po@8907987]    >> msgstr: 命令的退出码res 应该 < 0 或 >= 128"
+level=warning
+level=warning msg="commit <OID>: author (A U Thor <author@example.com>) and committer (C O Mitter <committer@example.com>) are different"
+level=error msg="checking commits: 1 passed, 1 failed."
+
+ERROR: fail to execute "git-po-helper check-commits"
+EOF
+
+test_expect_success "check-commits show typos and TEAMS file" '
+	(
+		cd workdir &&
+		test_must_fail $HELPER check-commits v1.. >out 2>&1 &&
+		make_user_friendly_and_stable_output <out >actual &&
+		test_cmp expect actual
+	)
+'
+
+test_done
