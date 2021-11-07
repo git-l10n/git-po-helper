7b6c2e15a000d32beb89423acccd368133c94bb4
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2021-11-07 20:16:57 +0800

    dict: more entries for smudge table of bg language
    See: https://github.com/git-l10n/git-po/pull/579
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- dict/smudge-bg.go
+++ dict/smudge-bg.go
@@ -2,9 +2,46 @@ package dict
 
 func init() {
 	SmudgeMaps["bg"] = map[interface{}]string{
-		"новият индекс": "new_index",
-		"зареждане на разширенията на индекса": "load_index_extensions",
-		"зареждане на обектите от кеша":        "load_cache_entries",
-		"--dirstat=ФАЙЛОВЕ":                    "--dirstat=files",
+		"———": "---",
+
+		// Not keep symbols "<" and ">", the reason is ?
+		"РЕГУЛЯРЕН_ИЗРАЗ":        "<РЕГУЛЯРЕН_ИЗРАЗ>",
+		"ДИРЕКТОРИЯ":             "<ДИРЕКТОРИЯ>",
+		"ПАКЕТЕН_ФАЙЛ":           "<ПАКЕТЕН_ФАЙЛ>",
+		"align:ШИРОЧИНА,ПОЗИЦИЯ": "align:<ШИРОЧИНА>,<ПОЗИЦИЯ>",
+		"color:ЦВЯТ":             "color:<ЦВЯТ>",
+		"--config=НАСТРОЙКА":     "--config=<НАСТРОЙКА>",
+		"--prefix=ПРЕФИКС":       "--prefix=<ПРЕФИКС>",
+		"--index-output=ФАЙЛ":    "--index-output=<ФАЙЛ>",
+		"--extcmd=КОМАНДА":       "--extcmd=<КОМАНДА>",
+		"--tool=ПРОГРАМА":        "--tool=<ПРОГРАМА>",
+		"--schedule=ЧЕСТОТА":     "--schedule=<ЧЕСТОТА>",
+		// Upstream may need to add "<>" around "files"
+		"--dirstat=ФАЙЛОВЕ": "--dirstat=files",
+		"--dirstat=ФАЙЛ…,ПАРАМЕТЪР_1,ПАРАМЕТЪР_2,": "--dirstat=files,param1,param2",
+
+		// Email address
+		"ИМЕ@example.com":   "you@example.com",
+		"пенчо@example.com": "you@example.com",
+
+		// add or lost '--'
+		"неправилен параметър към опцията „--update“": "bad value for update parameter",
+		"включва опцията „--bare“ за голо хранилище":  "implies bare",
+		"„--hard“/„--mixed“/„--soft“":                 "--{hard,mixed,soft}",
+		// Upstream should add "--" ?
+		"не поддържа опцията „--force“":         "does not support 'force'",
+		"неправилна стойност за „--mirror“: %s": "unknown mirror argument: %s",
+
+		// Add or lost "git" before subcommand
+		"Командата „git pack-objects“":                                               "spawn pack-objects",
+		"„git-difftool“ изисква работно дърво или опцията „--no-index“":              "difftool requires worktree or --no-index",
+		"командата „git index-pack“ не завърши успешно":                              "index-pack died",
+		"Командата „git pack-objects“ не завърши успешно":                            "pack-objects died",
+		"указателят „%s“ не е бил включен поради опциите зададени на „git rev-list“": "ref '%s' is excluded by the rev-list options",
+
+		// Typos ?
+		"зареждане на разширенията на индекса":  "load_index_extensions",
+		"зареждане на обектите от кеша":         "load_cache_entries",
+		"новият индекс не може да бъде записан": "unable to write new_index file",
 	}
 }
--- test/t0037-typos-in-bg.sh
+++ test/t0037-typos-in-bg.sh
@@ -13,21 +13,9 @@ test_expect_success "checkout po-2.31.1" '
 
 cat >expect <<-\EOF
 level=info msg="[po/bg.po]    5104 translated messages."
-level=warning msg="[po/bg.po]    mismatch variable names: example.com"
-level=warning msg="[po/bg.po]    >> msgid: \n*** Please tell me who you are.\n\nRun\n\n git config --global user.email \"you@example.com\"\n git config --global user.name \"Your Name\"\n\nto set your account's default identity.\nOmit --global to set the identity only in this repository.\n\n"
-level=warning msg="[po/bg.po]    >> msgstr: \n●●● Въведете самоличност.\n\nИзпълнете:\n\n git config --global user.email \"ИМЕ@example.com\"\n git config --global user.name \"ВАШЕТО ИМЕ\"\n\nи въведете данни за себе си.\nАко пропуснете опцията „--global“, самоличността е само за текущото хранилище.\n\n"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: $HOME"
 level=warning msg="[po/bg.po]    >> msgid: $HOME not set"
 level=warning msg="[po/bg.po]    >> msgstr: променливата „HOME“ не е зададена"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --git-dir=, --work-tree="
-level=warning msg="[po/bg.po]    >> msgid: %s (or --work-tree=<directory>) not allowed without specifying %s (or --git-dir=<directory>)"
-level=warning msg="[po/bg.po]    >> msgstr: %s (или --work-tree=ДИРЕКТОРИЯ) изисква указването на %s (или --git-dir=ДИРЕКТОРИЯ)"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: new_index"
-level=warning msg="[po/bg.po]    >> msgid: %s: Unable to write new index file"
-level=warning msg="[po/bg.po]    >> msgstr: %s: новият индекс не може да бъде запазен"
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --abort"
 level=warning msg="[po/bg.po]    >> msgid: --abort but leave index and working tree alone"
@@ -49,10 +37,6 @@ level=warning msg="[po/bg.po]    mismatch variable names: --cached, --no-index"
 level=warning msg="[po/bg.po]    >> msgid: --no-index or --untracked cannot be used with revs"
 level=warning msg="[po/bg.po]    >> msgstr: опциите „--cached“ и „--untracked“ са несъвместими с версии."
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --hard, --mixed, --soft"
-level=warning msg="[po/bg.po]    >> msgid: --patch is incompatible with --{hard,mixed,soft}"
-level=warning msg="[po/bg.po]    >> msgstr: опцията „--patch“ е несъвместима с всяка от опциите „--hard“/„--mixed“/„--soft“"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --points-at"
 level=warning msg="[po/bg.po]    >> msgid: --points-at option is only allowed in list mode"
 level=warning msg="[po/bg.po]    >> msgstr: Опцията „-points-at“ изисква режим на списък."
@@ -73,10 +57,6 @@ level=warning msg="[po/bg.po]    mismatch variable names: --name-only, --only-in
 level=warning msg="[po/bg.po]    >> msgid: --trailer with --only-input does not make sense"
 level=warning msg="[po/bg.po]    >> msgstr: опцията „--trailer“ е несъвместима с „--name-only“"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --unpacked="
-level=warning msg="[po/bg.po]    >> msgid: --unpacked=<packfile> no longer supported"
-level=warning msg="[po/bg.po]    >> msgstr: опцията „--unpacked=ПАКЕТЕН_ФАЙЛ“ вече не се поддържа"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --worktre, --worktree"
 level=warning msg="[po/bg.po]    >> msgid: --worktree cannot be used with multiple working trees unless the config\nextension worktreeConfig is enabled. Please read \"CONFIGURATION FILE\"\nsection in \"git help worktree\" for details"
 level=warning msg="[po/bg.po]    >> msgstr: опцията „--worktre“ не приема множество работни дървета, преди\nвключването на разширението в настройките „worktreeConfig“. За\nповече информация вижте раздела „CONFIGURATION FILE“ в\n„git help worktree“"
@@ -84,10 +64,6 @@ level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --no-commit"
 level=warning msg="[po/bg.po]    >> msgid: Automatic merge went well; stopped before committing as requested\n"
 level=warning msg="[po/bg.po]    >> msgstr: Автоматичното сливане завърши успешно. Самото подаване не е извършено, защото бе зададена опцията „--no-commit“.\n"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: git pack-objects"
-level=warning msg="[po/bg.po]    >> msgid: Could not spawn pack-objects"
-level=warning msg="[po/bg.po]    >> msgstr: Командата „git pack-objects“ не може да бъде стартирана"
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: $BRANCH, $br"
 level=warning msg="[po/bg.po]    >> msgid: Git normally never creates a ref that ends with 40 hex characters\nbecause it will be ignored when you just specify 40-hex. These refs\nmay be created by mistake. For example,\n\n git switch -c $br $(git rev-parse ...)\n\nwhere \"$br\" is somehow empty and a 40-hex ref is created. Please\nexamine these refs and maybe delete them. Turn this message off by\nrunning \"git config advice.objectNameWarning false\""
@@ -101,14 +77,6 @@ level=warning msg="[po/bg.po]    mismatch variable names: --no-ff"
 level=warning msg="[po/bg.po]    >> msgid: Non-fast-forward commit does not make sense into an empty head"
 level=warning msg="[po/bg.po]    >> msgstr: Понеже върхът е без история, всички сливания са превъртания, не може да се извърши същинско сливане изисквано от опцията „--no-ff“"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: example.com"
-level=warning msg="[po/bg.po]    >> msgid: Your name and email address were configured automatically based\non your username and hostname. Please check that they are accurate.\nYou can suppress this message by setting them explicitly:\n\n git config --global user.name \"Your Name\"\n git config --global user.email you@example.com\n\nAfter doing this, you may fix the identity used for this commit with:\n\n git commit --amend --reset-author\n"
-level=warning msg="[po/bg.po]    >> msgstr: Името и адресът за е-поща са настроени автоматично на базата на името на\nпотребителя и името на машината. Проверете дали са верни. Можете да спрете\nтова съобщение като изрично зададете стойностите:\n\n git config --global user.name \"Вашето Име\"\n git config --global user.email пенчо@example.com\n\nСлед като направите това, можете да коригирате информацията за автора на\nтекущото подаване чрез:\n\n git commit --amend --reset-author\n"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --update"
-level=warning msg="[po/bg.po]    >> msgid: bad value for update parameter"
-level=warning msg="[po/bg.po]    >> msgstr: неправилен параметър към опцията „--update“"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --filter, --stdout"
 level=warning msg="[po/bg.po]    >> msgid: cannot use --filter without --stdout"
 level=warning msg="[po/bg.po]    >> msgstr: опцията „-filter“ изисква „-stdout“"
@@ -117,37 +85,21 @@ level=warning msg="[po/bg.po]    mismatch variable names: dimmed_zebra"
 level=warning msg="[po/bg.po]    >> msgid: color moved setting must be one of 'no', 'default', 'blocks', 'zebra', 'dimmed-zebra', 'plain'"
 level=warning msg="[po/bg.po]    >> msgstr: настройката за цвят за преместване трябва да е една от: „no“ (без), „default“ (стандартно), „blocks“ (парчета), „zebra“ (райе), „dimmed_zebra“ (тъмно райе), „plain“ (обикновено)"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: conclude_pack"
+level=warning msg="[po/bg.po]    mismatch variable names: conclude_pack, vger.kernel.org"
 level=warning msg="[po/bg.po]    >> msgid: confusion beyond insanity"
 level=warning msg="[po/bg.po]    >> msgstr: фатална грешка във функцията „conclude_pack“. Това е грешка в Git, докладвайте я на разработчиците, като пратите е-писмо на адрес: „git@vger.kernel.org“."
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --bare"
-level=warning msg="[po/bg.po]    >> msgid: create a mirror repository (implies bare)"
-level=warning msg="[po/bg.po]    >> msgstr: създаване на хранилище-огледало (включва опцията „--bare“ за голо хранилище)"
+level=warning msg="[po/bg.po]    mismatch variable names: vger.kernel.org"
+level=warning msg="[po/bg.po]    >> msgid: confusion beyond insanity in parse_pack_objects()"
+level=warning msg="[po/bg.po]    >> msgstr: фатална грешка във функцията „parse_pack_objects“. Това е грешка в Git, докладвайте я на разработчиците, като пратите е-писмо на адрес: „git@vger.kernel.org“."
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: credential-cache--daemon"
 level=warning msg="[po/bg.po]    >> msgid: credential-cache--daemon unavailable; no unix socket support"
 level=warning msg="[po/bg.po]    >> msgstr: демонът за кеша с идентификациите е недостъпен — липсва поддръжка на гнезда на unix"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: git-difftool"
-level=warning msg="[po/bg.po]    >> msgid: difftool requires worktree or --no-index"
-level=warning msg="[po/bg.po]    >> msgstr: „git-difftool“ изисква работно дърво или опцията „--no-index“"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --word-diff-regex="
-level=warning msg="[po/bg.po]    >> msgid: equivalent to --word-diff=color --word-diff-regex=<regex>"
-level=warning msg="[po/bg.po]    >> msgstr: псевдоним на „--word-diff=color --word-diff-regex=РЕГУЛЯРЕН_ИЗРАЗ“"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: %%(trailers:key=<...>), %%(trailers:КЛЮЧ=СТОЙНОСТ)"
 level=warning msg="[po/bg.po]    >> msgid: expected %%(trailers:key=<value>)"
 level=warning msg="[po/bg.po]    >> msgstr: очаква се %%(trailers:КЛЮЧ=СТОЙНОСТ)"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: %%(align:<...>,<...>), %%(align:ШИРОЧИНА,ПОЗИЦИЯ)"
-level=warning msg="[po/bg.po]    >> msgid: expected format: %%(align:<width>,<position>)"
-level=warning msg="[po/bg.po]    >> msgstr: очакван формат: %%(align:ШИРОЧИНА,ПОЗИЦИЯ)"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: %%(color:<...>), %%(color:ЦВЯТ)"
-level=warning msg="[po/bg.po]    >> msgid: expected format: %%(color:<color>)"
-level=warning msg="[po/bg.po]    >> msgstr: очакван формат: %%(color:ЦВЯТ)"
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --quit, --skip"
 level=warning msg="[po/bg.po]    >> msgid: git am [<options>] (--continue | --skip | --abort)"
@@ -169,17 +121,9 @@ level=warning msg="[po/bg.po]    mismatch variable names: --bisect-reset, --bise
 level=warning msg="[po/bg.po]    >> msgid: git bisect--helper --bisect-state (good|old) [<rev>...]"
 level=warning msg="[po/bg.po]    >> msgstr: git bisect--helper --bisect-reset (ДОБРО) [ВЕРСИЯ…]"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --config="
-level=warning msg="[po/bg.po]    >> msgid: git for-each-repo --config=<config> <command-args>"
-level=warning msg="[po/bg.po]    >> msgstr: git for-each-repo --config=НАСТРОЙКА АРГУМЕНТ…"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: --batch-size="
 level=warning msg="[po/bg.po]    >> msgid: git multi-pack-index [<options>] (write|verify|expire|repack --batch-size=<size>)"
 level=warning msg="[po/bg.po]    >> msgstr: git multi-pack-index [ОПЦИЯ…] (write|verify|expire|repack --batch-size=РАЗМЕР)"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --prefix="
-level=warning msg="[po/bg.po]    >> msgid: git read-tree [(-m [--trivial] [--aggressive] | --reset | --prefix=<prefix>) [-u [--exclude-per-directory=<gitignore>] | -i]] [--no-sparse-checkout] [--index-output=<file>] (--empty | <tree-ish1> [<tree-ish2> [<tree-ish3>]])"
-level=warning msg="[po/bg.po]    >> msgstr: git read-tree [(-m [--trivial] [--aggressive] | --reset | --prefix=ПРЕФИКС) [-u [--exclude-per-directory=ФАЙЛ_С_ИЗКЛЮЧЕНИЯ] | -i]] [--no-sparse-checkout] [--index-output=ФАЙЛ] (--empty | УКАЗАТЕЛ_КЪМ_ДЪРВО_1 [УКАЗАТЕЛ_КЪМ_ДЪРВО_2 [УКАЗАТЕЛ_КЪМ_ДЪРВО_3]])"
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: git upload-pack, git upload-repack"
 level=warning msg="[po/bg.po]    >> msgid: git upload-pack [<options>] <dir>"
@@ -197,37 +141,9 @@ level=warning msg="[po/bg.po]    mismatch variable names: crlf_action"
 level=warning msg="[po/bg.po]    >> msgid: illegal crlf_action %d"
 level=warning msg="[po/bg.po]    >> msgstr: неправилно действие за край на ред: %d"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: git index-pack"
-level=warning msg="[po/bg.po]    >> msgid: index-pack died"
-level=warning msg="[po/bg.po]    >> msgstr: командата „git index-pack“ не завърши успешно"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: s.merge"
 level=warning msg="[po/bg.po]    >> msgid: invalid branch.%s.merge; cannot rebase onto > 1 branch"
 level=warning msg="[po/bg.po]    >> msgstr: неправилен клон за сливане „%s“. Невъзможно е да пребазирате върху повече от 1 клон"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: new_index"
-level=warning msg="[po/bg.po]    >> msgid: merge: Unable to write new index file"
-level=warning msg="[po/bg.po]    >> msgstr: сливане: новият индекс не може да бъде запазен"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --config="
-level=warning msg="[po/bg.po]    >> msgid: missing --config=<config>"
-level=warning msg="[po/bg.po]    >> msgstr: липсва --config=НАСТРОЙКА"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --extcmd="
-level=warning msg="[po/bg.po]    >> msgid: no <cmd> given for --extcmd=<cmd>"
-level=warning msg="[po/bg.po]    >> msgstr: не е зададена команда за „--extcmd=КОМАНДА“"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --tool="
-level=warning msg="[po/bg.po]    >> msgid: no <tool> given for --tool=<tool>"
-level=warning msg="[po/bg.po]    >> msgstr: не е зададена програма за „--tool=ПРОГРАМА“"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: git pack-objects"
-level=warning msg="[po/bg.po]    >> msgid: pack-objects died"
-level=warning msg="[po/bg.po]    >> msgstr: Командата „git pack-objects“ не завърши успешно"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: git rev-list"
-level=warning msg="[po/bg.po]    >> msgid: ref '%s' is excluded by the rev-list options"
-level=warning msg="[po/bg.po]    >> msgstr: указателят „%s“ не е бил включен поради опциите зададени на „git rev-list“"
 level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: run_command"
 level=warning msg="[po/bg.po]    >> msgid: run_command returned non-zero status for %s\n."
@@ -261,10 +177,6 @@ level=warning msg="[po/bg.po]    mismatch variable names: --patch"
 level=warning msg="[po/bg.po]    >> msgid: unknown --patch mode: %s"
 level=warning msg="[po/bg.po]    >> msgstr: неизвестна стратегия за прилагане на кръпка: „%s“"
 level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --mirror"
-level=warning msg="[po/bg.po]    >> msgid: unknown mirror argument: %s"
-level=warning msg="[po/bg.po]    >> msgstr: неправилна стойност за „--mirror“: %s"
-level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: update_ref"
 level=warning msg="[po/bg.po]    >> msgid: update_ref failed for ref '%s': %s"
 level=warning msg="[po/bg.po]    >> msgstr: неуспешно обновяване на указателя „%s“: %s"
@@ -272,10 +184,6 @@ level=warning
 level=warning msg="[po/bg.po]    mismatch variable names: git merge-base"
 level=warning msg="[po/bg.po]    >> msgid: use 'merge-base --fork-point' to refine upstream"
 level=warning msg="[po/bg.po]    >> msgstr: за доуточняването на следения клон, използвайте:\n\n git merge-base --fork-point"
-level=warning
-level=warning msg="[po/bg.po]    mismatch variable names: --schedule="
-level=warning msg="[po/bg.po]    >> msgid: use at most one of --auto and --schedule=<frequency>"
-level=warning msg="[po/bg.po]    >> msgstr: може да се указва максимум една от опциите „--auto“ и „--schedule=ЧЕСТОТА“"
 level=warning
 EOF
 
